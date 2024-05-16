d55dc41ee4f039375ad275010e52a34f7501d6a3
Heitor0308 <heitorferrari381@gmail.com>
2024-05-15 23:41:38 -0300

    first commit

---
+++ AMST4.Carousel.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using AMST4.Carousel.MVC.Context;
+using AMST4.Carousel.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMST4.Carousel.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDataContext _applicationDataContext;
+        public CategoryController(ApplicationDataContext applicationDataContext)
+        {
+            _applicationDataContext = applicationDataContext;
+        }
+        public IActionResult CategoryList()
+        {
+            var categories = _applicationDataContext.Category.ToList();
+
+            return View(categories);
+        }
+
+        [HttpPost]
+
+        public IActionResult AddCategory(Category category)
+        {
+            _applicationDataContext.Category.Add(category);
+            _applicationDataContext.SaveChanges();
+            return RedirectToAction("CategoryList");
+
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ AMST4.Carousel.MVC/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AMST4.Carousel.MVC.Context;
+using AMST4.Carousel.MVC.Models;
+
+namespace AMST4.Carousel.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDataContext _context;
+
+        public ProductController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ProductList()
+        {
+            var product = _context.Product.ToList();
+            return View(product);
+        }
+
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+
+            product.Id = Guid.NewGuid();
+            _context.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+
+    }
+}
